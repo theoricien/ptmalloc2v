a0cf6267940fda41663b5708d67b5a93d0669afc
theoricien <letheoricien@hotmail.com>
2020-06-10 22:13:18 +0200

    main_arena retrievment try

--- src/heapview.c
+++ src/heapview.c
@@ -1,5 +1,21 @@
 #include "heapview.h"
 
+/**
+ * heapview.c file regroup the 3 functions heap_init, heap_view and heap_end.
+ * heap_init functions is here to retrieve informations about the heap, like
+ * the start of the heap data, the static variables that we cannot access to
+ * normally, and other stuff that catch heap informations.
+ *
+ * heap_view consist of display all informations about the current heap. The
+ * goal of this function is to display the more pertinent information to the
+ * less one, like displaying main_arena in first, and at the end display
+ * macros values.
+ *
+ * heap_end is just free()-ing all the stuff allocated by heap_init.
+ */
+
+#define OFF_MAIN_ARENA 0x46150
+
 struct ptm2v_info *
 heap_init (void)
 {
@@ -22,8 +38,16 @@ int
 heap_view (struct ptm2v_info * info)
 {
     int    err;
+    void   *(*r_malloc) (size_t);
+    void   *main_arena;
 
     err = 0;
+    r_malloc = NULL;
+    r_malloc = dlsym(RTDL_NEXT, "malloc");
+    ptassertdl(r_malloc != NULL);
+
+    main_arena = r_malloc + OFF_MAIN_ARENA;
+    printf("malloc: %p\nmain_arena: %p\n", r_malloc, main_arena);
     for (addr_t ptr = info->heap_base; ptr < info->heap_base + 1000; ptr += sizeof(addr_t))
     {
         print_mem(ptr, (addr_t) *(long long *)ptr);
